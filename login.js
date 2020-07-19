68a6cf4253b657e8e0fb2bc896ce0f4d86527d29
Luke Hoang <lukehoang195@gmail.com>
2020-07-19 23:04:34 +0700

    login page done , profile pic retrieve done

--- login.js
+++ login.js
@@ -1,9 +1,15 @@
+//clear all storage when reload this page
+window.localStorage.clear();
+
 let attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
+
 const test = { username: 'tester', password: 'abc' }
+let user = []
+
+let userDataBase = [{ userName: 'tester', userPassword: 'abc', userEmail: 'test@gmail.com', userPic: "#", userLink: "" }]
 
 
-let userDataBase = [{ userName: 'tester', userPassword: 'abc', userEmail: 'test@gmail.com', userPic: "#", userLink: "##" }]
 
 function validate() {
     let usernameInput = document.getElementById("username").value;
@@ -11,7 +17,7 @@ function validate() {
 
     for (i = 0; i < userDataBase.length; i++) {
         if (usernameInput == userDataBase[i].userName && passwordInput == userDataBase[i].userPassword) {
-            alert("Login successfully");
+            alert("Log-in successfully");
             window.location = "home.html"; // Redirecting to other page.
 
         } else {
@@ -34,14 +40,6 @@ const check = () => {
 }
 
 
-let user = []
-
-
-
-
-
-
-
 
 
 const signUp = () => {
@@ -51,25 +49,73 @@ const signUp = () => {
     }
 
     aUserName = document.getElementById('signUser').value
+    checkUserName(aUserName)
 
     aUserEmail = document.getElementById('signEmail').value
-
-    // if(userDataBase.map((data, index) => data.userName).includes(aUserName)===true ||
-    // userDataBase.map((data, index) => data.userPassword).includes(aUserEmail)===false)
-
+    checkUserEmail(aUserEmail)
     aUserPassword = document.getElementById('signPassword').value
-    aUserPic = 'default'
+    checkPassword(aUserPassword)
+    aUserPic = JSON.parse(window.localStorage.getItem('userPic'))
 
-    let newUser = {
-        'userName': aUserName,
-        'userPassword': aUserPassword,
-        'userEmail': aUserEmail,
-        'userPic': "#",
-        'userLink': "##"
+    document.getElementById('out').setAttribute(`src`, aUserPic)
+
+
+    if (checkUserName(aUserName) === true && checkPassword(aUserPassword) === true && checkUserEmail(aUserEmail) === true) {
+        let newUser = {
+            'userName': aUserName,
+            'userPassword': aUserPassword,
+            'userEmail': aUserEmail,
+            'userPic': aUserPic,
+            'userLink': `@${aUserName}`
+        }
+
+        userDataBase.push(newUser)
+
+        //import onto
+        window.localStorage.setItem('user', JSON.stringify(newUser));
+        alert("sign up successful")
+    } else {
+        alert("sign up unsuccessful")
+
     }
-
-    userDataBase.push(newUser)
-
     console.log(userDataBase)
+    console.log(JSON.parse(window.localStorage.getItem('user')))
+    return
+}
 
+
+
+
+const checkPassword = (password) => {
+    if (password.length < 8) {
+        alert("password must be at least 8 characters")
+        return false
+    }
+    if (!password.match(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%* #+=\(\)\^?&])[A-Za-z\d$@$!%* #+=\(\)\^?&]{3,}$/)) {
+        alert("password must include at least 1 number and 1 letter and 1 special character")
+        return false
+    }
+    return true
+
+};
+
+const checkUserName = (a) => {
+    for (i = 0; i < userDataBase.length; i++) {
+        if (userDataBase[i].userName == a) {
+            console.log("Username already taken")
+            return false
+        }
+    }
+    alert('You can use this Username')
+    return true
+}
+const checkUserEmail = (a) => {
+    for (i = 0; i < userDataBase.length; i++) {
+        if (userDataBase[i].userEmail == a) {
+            console.log("email already taken")
+            return false
+        }
+    }
+    alert('You can use this Email')
+    return true
 }
